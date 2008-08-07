1906fd617a446283e8238771413f52ab99414245
Yossef Mendelssohn <ymendel@pobox.com>
2008-08-07 17:22:46 -0500

    Refactoring, condensing truthiness code.

--- lib/truthy.rb
+++ lib/truthy.rb
@@ -1,20 +1,8 @@
 $:.unshift(File.dirname(__FILE__)) unless
   $:.include?(File.dirname(__FILE__)) || $:.include?(File.expand_path(File.dirname(__FILE__)))
 
-class FalseClass
-  def truthy?
-    false
-  end
-end
-
-class NilClass
-  def truthy?
-    false
-  end
-end
-
 class Object
   def truthy?
-    true
+    !!self
   end
 end
