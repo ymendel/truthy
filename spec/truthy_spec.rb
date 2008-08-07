1839067b334ca377452c1dd87e613bf9a220a8f1
Yossef Mendelssohn <ymendel@pobox.com>
2008-08-07 17:12:01 -0500

    Initial spec for truthiness of false.

--- spec/truthy_spec.rb
+++ spec/truthy_spec.rb
@@ -1,11 +1,9 @@
 require File.dirname(__FILE__) + '/spec_helper.rb'
 
-# Time to add your specs!
-# http://rspec.info/
-describe "Place your specs here" do
+describe 'truthiness' do
   
-  it "find this spec in spec directory" do
-    violated "Be sure to write your specs"
+  specify 'false should not be truthy' do
+    false.should_not be_truthy
   end
   
 end
